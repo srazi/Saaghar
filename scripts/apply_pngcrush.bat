4823c404fc3ac85d6839c193d7c36e649765a0d1
srazi <srazi61@yahoo.com>
2016-07-29 20:35:28 +0430

    script: fix apply_pngcrush.bat

--- scripts/apply_pngcrush.bat
+++ scripts/apply_pngcrush.bat
@@ -1,15 +1,16 @@
 @echo off
 
 set SAAGHAR_SRC=".."
-set PNG_CRUSH="./pngcrush"
+set PNG_CRUSH="pngcrush.exe"
 
 pushd %SAAGHAR_SRC%
-for /r %%x in (*.png) do call :convert %%x
+for /r %%x in (*.png) do call :convert %%x %%~nx
 
 GOTO :finish
 
 :convert
 set inFile="%~1"
+set inFileName="%~2.png"
 set tempFile="%~1.2"
 echo ~~~~~~~~~~~~~~~~~~~~
 echo %inFile%
@@ -18,6 +19,7 @@ echo ~~~~~~~~~~~~~~~~~~~~
 
 %PNG_CRUSH% %inFile% %tempFile%
 del %inFile%
+rename %tempFile% %inFileName%
 
 :finish
 
